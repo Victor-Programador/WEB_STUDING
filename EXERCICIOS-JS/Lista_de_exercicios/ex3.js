2b442778d000c3b756628711fdd35c48e838c5a0
Victor Santos <vitthugo1234@gmail.com>
2024-09-07 00:04:57 -0300

    Exercicio 3

---
+++ EXERCICIOS-JS/Lista_de_exercicios/ex3.js
@@ -0,0 +1,7 @@
+/*03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+function Potencia(base, expoente){
+    console.log(`${base} elevado a ${expoente} é igual a: ${base ** expoente}`)
+}
+
+Potencia(2, 4)
