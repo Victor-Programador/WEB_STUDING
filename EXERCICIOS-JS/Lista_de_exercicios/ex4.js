e8a300ec9cf5c72cb4e973a112e5f96ffa801535
Victor Santos <vitthugo1234@gmail.com>
2024-09-07 00:38:18 -0300

    Exercicio 4

---
+++ EXERCICIOS-JS/Lista_de_exercicios/ex4.js
@@ -0,0 +1,8 @@
+/*04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.*/
+
+function dividir(dividendo, divisor){
+    console.log(`${dividendo} dividido por ${divisor} é igual a ${dividendo / divisor} tendo resto ${dividendo % divisor}`)
+}
+
+dividir(4,3)
